9529a7496f7556fc945a946abbb3ca7067b9cdbb
npereira94 <60931187+npereira94@users.noreply.github.com>
2020-04-28 08:51:17 +0200

    Motif 6

--- 6/sketch.js
+++ 6/sketch.js
@@ -1,17 +1,31 @@
 function setup() {
   let navWidth = document.getElementById("nav").offsetWidth;
   let canvas = createCanvas(windowWidth - navWidth, windowHeight);
+  let x = 0;
   canvas.parent('canvas');
   background(255);
+  noLoop();
 }
 
 function draw() {
-  fill(0);
-  for (let x = 10; x < width; x = x + 30) {
-    ellipse(x, height / 2, 10, 10);
+  background(204);
+  x = x + 100;
+  if (x > width) {
+    x = 0;
   }
+  line(x, 200, x, height);
 }
 
+
+function mousePressed() {
+  loop();
+}
+
+function mouseReleased() {
+  noLoop();
+}
+
+horizontal line moves slowly from left.Loops but stops on mouse press.
 function windowResized() {
   let navWidth = document.getElementById("nav").offsetWidth;
   resizeCanvas(windowWidth - navWidth, windowHeight);
