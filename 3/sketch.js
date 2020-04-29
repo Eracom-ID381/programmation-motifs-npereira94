3fb3e05951369d85b258685888f039f66183e787
npereira94 <60931187+npereira94@users.noreply.github.com>
2020-04-29 14:39:26 +0200

    Motif 3

--- 3/sketch.js
+++ 3/sketch.js
@@ -12,11 +12,30 @@ function draw() {
   fill(0);
   for (let x = 10; x <= width - 20; x += 10) {
     for (let y = 100; y <= height - 100; y += 100) {
-      line(0, x, x, 1000);
-      line(1000, x, x, 0);
-      line(0, x, x, 0);
 
+      line(1, x, x, 100);
+      line(100, x, x, 1);
+      line(1, x, x, 1);
 
+      line(1, x, x, 250);
+      line(250, x, x, 1);
+      line(1, x, x, 1);
+
+      line(1, x, x, 500);
+      line(500, x, x, 1);
+      line(1, x, x, 1);
+
+      line(x, 1, 1000, x);
+      line(x, 1000, 1, x);
+      line(1, y, y, 1);
+
+      line(x, 1, 1500, x);
+      line(x, 1500, 1, x);
+      line(1, y, y, 1);
+
+      line(x, 1, 2000, x);
+      line(x, 2000, 1, x);
+      line(1, y, y, 1);
     }
   }
 }
